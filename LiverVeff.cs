dd9f57845cbd7387d0775b02a7618dbdeab1aa91
jkeener9 <justin.keener9@gmail.com>
2019-04-16 14:14:07 -0600

    Add project files.

---
+++ LiverVeff.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+
+// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+// TODO: Uncomment the following line if the script requires write access.
+// [assembly: ESAPIScript(IsWriteable = true)]
+
+namespace VMS.TPS
+{
+  public class Script
+  {
+        public Script()
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        
+
+        public void Execute(ScriptContext scriptContext, Window mainWindow /*, System.Windows.Window window, ScriptEnvironment environment*/)
+        {
+            Run(scriptContext.CurrentUser,
+              scriptContext.Patient,
+              scriptContext.Image,
+              scriptContext.StructureSet,
+              scriptContext.PlanSetup,
+              scriptContext.PlansInScope,
+              scriptContext.PlanSumsInScope,
+              mainWindow);
+        }
+
+        public void Run(
+            User user,
+            Patient patient,
+            Image image,
+            StructureSet structureSet,
+            PlanSetup planSetup,
+            IEnumerable<PlanSetup> planSetupsInScope,
+            IEnumerable<PlanSum> planSumsInScope,
+            Window window)
+
+        {
+           
+            // If there's no selected plan with calculated dose throw an exception
+            if (planSetup == null || planSetup.Dose == null)
+                throw new ApplicationException("Please open a calculated plan before using this script.");
+
+            // Retrieve StructureSet
+            structureSet = planSetup.StructureSet;
+            if (structureSet == null)
+                throw new ApplicationException("The selected plan does not reference a StructureSet.");
+
+            //****** will need to be Liver-GTV   ************** //
+            Structure Liver = structureSet.Structures.Where(s => s.Id.Equals("Liver")).Single();    
+            
+
+            // Retrieve DVH data
+            DVHData dvhData = planSetup.GetDVHCumulativeData(Liver,
+                                          DoseValuePresentation.Absolute,
+                                          VolumePresentation.Relative, 1);  //can decrease resolution to .1 if desired
+
+
+            //Reformat DVH data into array
+            int nbins = dvhData.CurveData.Length - 1;
+            double[] dvhdosecGy = new double[nbins];
+            double[] dvhvolumepercent = new double[nbins];
+            for (int i = 0; i < nbins; i++)
+            {
+                dvhdosecGy[i] = dvhData.CurveData[i].DoseValue.Dose;
+                dvhvolumepercent[i] = dvhData.CurveData[i].Volume;
+            }
+
+            //Convert Cumulative DVH to Differential DVH
+            double[] DiffdvhdosecGy = dvhdosecGy;
+            double[] Diffdvhvolumepercent = new double[nbins];
+            for (int i = 0; i < nbins - 1; i++)
+            {
+                Diffdvhvolumepercent[i] = dvhvolumepercent[i] - dvhvolumepercent[i + 1];  //Reference Clinical Radiotherapy Physics with MATLAB: A Problem-Solving Approach By Pavel Dvorak
+            }
+
+            if (dvhData == null)
+                throw new ApplicationException("DVH data does not exist. Script execution cancelled.");
+
+            /*
+            // Add existing WPF control to the script window.
+            var mainControl = new LiverVeff.MainControl();
+            window.Content = mainControl;
+            window.Width = 610;
+            window.Height = 460;
+
+            window.Title = "Plan : " + planSetup.Id + ", Structure : " + Liver.Id;
+
+            // Draw DVH
+            mainControl.DrawDVH(dvhData);
+            mainControl.DrawDiffDVH(DiffdvhdosecGy, Diffdvhvolumepercent);
+
+            */
+
+            //Mean Liver Dose
+            var MeanLiverDose = dvhData.MeanDose;
+
+
+            //Compute Veff
+            double Dref_cGy = planSetup.TotalPrescribedDose.Dose;
+            double Veffpercent = 0;
+            for (int i = 0; i < nbins - 1; i++)
+            {
+                Veffpercent = Veffpercent + Diffdvhvolumepercent[i] * DiffdvhdosecGy[i] / Dref_cGy;    //Reference RTOG 1112
+            }
+
+
+
+            /*
+
+            //Iterative Prescription dose reduction
+            if (Veffpercent > 25)
+            {
+                DrefNEW_cGy = 4500;
+                double VeffNEWpercent = 0;
+                for (int i = 0; i < nbins - 1; i++)
+                {
+                    VeffNEWpercentNEW = VeffNEWpercent + Diffdvhvolumepercent[i] * DiffdvhdosecGy[i] / DrefNEW_cGy;
+                }
+
+                if (VeffNEWpercent > 29)
+                {
+                    DrefNEW_cGy = 4000;
+                    double VeffNEWpercent = 0;
+                    for (int i = 0; i < nbins - 1; i++)
+                    {
+                        VeffNEWpercentNEW = VeffNEWpercent + Diffdvhvolumepercent[i] * DiffdvhdosecGy[i] / DrefNEW_cGy;
+                    }
+
+                    if (VeffNEWpercent > 34)
+                    {
+                        DrefNEW_cGy = 3500;
+                        double VeffNEWpercent = 0;
+                        for (int i = 0; i < nbins - 1; i++)
+                        {
+                            VeffNEWpercentNEW = VeffNEWpercent + Diffdvhvolumepercent[i] * DiffdvhdosecGy[i] / DrefNEW_cGy;
+                        }
+
+                        if (VeffNEWpercent > 44)
+                        {
+                            DrefNEW_cGy = 3000;
+                            double VeffNEWpercent = 0;
+                            for (int i = 0; i < nbins - 1; i++)
+                            {
+                                VeffNEWpercentNEW = VeffNEWpercent + Diffdvhvolumepercent[i] * DiffdvhdosecGy[i] / DrefNEW_cGy;
+                            }
+
+                            if (VeffNEWpercent > 54)
+                            {
+                                DrefNEW_cGy = 2750;
+                                double VeffNEWpercent = 0;
+                                for (int i = 0; i < nbins - 1; i++)
+                                {
+                                    VeffNEWpercentNEW = VeffNEWpercent + Diffdvhvolumepercent[i] * DiffdvhdosecGy[i] / DrefNEW_cGy;
+                                }
+                                if (VeffNEWpercent > 64)
+                                {
+                                    string Flag = "ineligible";
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+           */
+                
+
+
+
+
+            //Report Calculation results
+            string msg = "Veff = " + Veffpercent.ToString() + "%" + "\n" 
+                + "Dref = " + Dref_cGy.ToString() + " cGy" + "\n"
+                + "Mean Liver Dose = " + MeanLiverDose.ToString();
+
+                           
+            MessageBox.Show(msg);
+        }
+  }
+}
+
---
+++ MainControl.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VMS.TPS.Common.Model.API;
+
+namespace LiverVeff
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class MainControl : UserControl
+    {
+        public MainControl()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawDVH(DVHData dvhData)
+        {
+            // Calculate multipliers for scaling DVH to canvas.
+            double xCoeff = MainCanvas.Width / dvhData.MaxDose.Dose;
+            double yCoeff = MainCanvas.Height / 100;
+
+            // Set Y axis label
+            //DoseMaxLabel.Content = string.Format("{0:F0}%", dvhData.MaxDose.Dose);
+
+            // Draw  histogram 
+            for (int i = 0; i < dvhData.CurveData.Length - 1; i++)
+            {
+                // Set drawing line parameters
+                var line = new Line() { Stroke = Brushes.Blue, StrokeThickness = 3.0 };
+
+                // Set line coordinates
+                line.X1 = dvhData.CurveData[i].DoseValue.Dose * xCoeff;
+                line.X2 = dvhData.CurveData[i + 1].DoseValue.Dose * xCoeff;
+                // Y axis start point is top-left corner of window, convert it to bottom-left.
+                line.Y1 = MainCanvas.Height - dvhData.CurveData[i].Volume * yCoeff;
+                line.Y2 = MainCanvas.Height - dvhData.CurveData[i + 1].Volume * yCoeff;
+
+                // Add line to the existing canvas
+                MainCanvas.Children.Add(line);
+            }
+        }
+        public void DrawDiffDVH(double[] DiffdvhdosecGy, double[] Diffdvhvolumepercent)
+        {
+            int bins = DiffdvhdosecGy.Length - 1;
+            // Calculate multipliers for scaling DVH to canvas.
+            double xCoeff = MainCanvas.Width / DiffdvhdosecGy[bins];
+            double yCoeff = MainCanvas.Height / 100;
+
+           
+            // Draw  histogram 
+            for (int i = 0; i < bins - 1; i++)
+            {
+                // Set drawing line parameters
+                var line = new Line() { Stroke = Brushes.Red, StrokeThickness = 3.0 };
+
+                // Set line coordinates
+                line.X1 = DiffdvhdosecGy[i] * xCoeff;
+                line.X2 = DiffdvhdosecGy[i + 1] * xCoeff;
+                // Y axis start point is top-left corner of window, convert it to bottom-left.
+                line.Y1 = MainCanvas.Height - Diffdvhvolumepercent[i] * yCoeff;
+                line.Y2 = MainCanvas.Height - Diffdvhvolumepercent[i + 1] * yCoeff;
+
+                // Add line to the existing canvas
+                MainCanvas.Children.Add(line);
+            }
+        }
+    }
+}
